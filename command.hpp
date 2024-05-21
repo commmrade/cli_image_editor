1e1e37f2542e841e5030e0edbb11a8da2720dd5b
klewy <cigankov807@gmail.com>
2024-05-21 16:38:12 +1000

    minor fix

--- command.hpp
+++ command.hpp
@@ -1,21 +1,6 @@
 #pragma once
 #include <iostream>
-#include<opencv2/core.hpp>
 //#include<opencv4/opencv2/opencv.hpp>
-#include <opencv2/core/cvstd.hpp>
-#include <opencv2/core/hal/interface.h>
-#include <opencv2/core/mat.hpp>
-#include <opencv2/core/matx.hpp>
-#include <opencv2/core/utility.hpp>
-#include <opencv2/highgui.hpp>
-#include <opencv2/imgproc.hpp>
-#include<opencv2/opencv.hpp>
-#include<ranges>
-#include<format>
-#include<opencv2/imgcodecs.hpp>
-#include <sys/types.h>
-
-
 
 enum Action
 {
--- main.cpp
+++ main.cpp
@@ -1,15 +1,6 @@
 #include <cstdlib>
-#include <exception>
 #include <iostream>
 #include<opencv2/core.hpp>
-//#include<opencv4/opencv2/opencv.hpp>
-#include <opencv2/core/hal/interface.h>
-#include <opencv2/core/mat.hpp>
-#include <opencv2/core/matx.hpp>
-#include <opencv2/core/utility.hpp>
-#include <opencv2/highgui.hpp>
-#include <opencv2/imgproc.hpp>
-#include<opencv2/opencv.hpp>
 #include<ranges>
 #include"command.hpp"
 #include<format>
